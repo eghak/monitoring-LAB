2139f7da34a11543316d4f3e5d833f077d275fb2
Egha Kusuma <eghakusuma1@gmail.com>
2021-09-14 12:59:55 -0700

    update

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+const { cargo } = require("async");
 const express = require(`express`);
 const path = require(`path`);
-// const Rollbar = require(`rollbar`);
+const Rollbar = require(`rollbar`);
 
-// let rollbar = new Rollbar({
-//     accessToken: ``,
-//     captureUncaught: true,
-//     captureUnhandledRejections: true,
-//   });
+let rollbar = new Rollbar({
+  accessToken: `9c9e8f6ee3934079a6d44aca8e61e448`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
-const app = express()
+const app = express();
 
 app.get(`/`, (req, res) => {
   res.sendFile(path.join(__dirname, `/public/index.html`));
-//   Rollbar.info(`HTML file served successfully!`)
+  Rollbar.info(`HTML file served successfully!`);
+});
+
+app.get(`api/lab`, (req, res) => {
+  try {
+    car();
+  } catch (error) {
+    rollbar.error(`error`);
+  }
 });
 
 const port = process.env.PORT || 4005;
